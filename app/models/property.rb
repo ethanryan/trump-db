afd95bf03f25dbead664900c8a525df89b58bd26
ethanryan <ethanryan@gmail.com>
2018-04-01 18:51:43 -0400

    updated models with has_many and belongs_to relationships

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,4 @@
 class Property < ApplicationRecord
+  has_many :windows
+  has_many :ads, through: :windows
 end
--- app/models/window.rb
+++ app/models/window.rb
@@ -1,2 +1,4 @@
 class Window < ApplicationRecord
+  belongs_to :property
+  has_many :ads
 end
